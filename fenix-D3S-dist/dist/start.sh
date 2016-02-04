e0d3d8cdc9978530c0d48e5b1504f89e906ac66e
Ivano Cortesini <ivano.cortesini@gmail.com>
2016-02-04 16:01:01 +0100

    ...

--- fenix-D3S-dist/dist/start.sh
+++ fenix-D3S-dist/dist/start.sh
@@ -1,4 +1,5 @@
 JAVA_OPTS="-server -Xmx2g -Djna.nosys=true -XX:+HeapDumpOnOutOfMemoryError -Djava.awt.headless=true -Dfile.encoding=UTF8 -Drhino.opt.level=9"
 ORIENTDB_SETTINGS="-Dprofiler.enabled=true -Dorientdb.www.path="database/www""
+LOG_SETTINGS="-Dlog4j.configuration="file:config/logging/log4j.properties""
 
 java $JAVA_OPTS $ORIENTDB_SETTINGS -jar lib/d3s.jar &
