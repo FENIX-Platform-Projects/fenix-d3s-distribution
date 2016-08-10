79e37518c1e90ee23240f75f54b228e656efc5e7
fabriziofenix <fabrizio.castelli@fao.org>
2016-08-10 18:33:47 +0200

    update on #1: policy process configuration #2: dataset configuration

--- fenix-D3S-policy/src/main/java/org/fao/policy/d3s/PolicyViewsCacheListener.java
+++ fenix-D3S-policy/src/main/java/org/fao/policy/d3s/PolicyViewsCacheListener.java
@@ -12,7 +12,44 @@ public class PolicyViewsCacheListener implements DatasetCacheListener {
         //Browse Data indexes
         //datasetInfo.getConnection().createStatement().executeUpdate("create index on " + datasetInfo.getTableName() + " ( oda,sectorcode, purposecode)");
 
-        System.out.println("here");
+        String uid = datasetInfo.getMetadata().getUid();
+
+
+        if ("OECD_View_ImportTariffs".equals(uid)) {
+            datasetInfo.getConnection().createStatement().executeUpdate("create index on " + datasetInfo.getTableName() + " ( hsPrefix,policytype)");
+            datasetInfo.getConnection().createStatement().executeUpdate("create index on " + datasetInfo.getTableName() + " ( commodityclass,policytype)");
+            datasetInfo.getConnection().createStatement().executeUpdate("create index on " + datasetInfo.getTableName() + " ( policytype,country)");
+            datasetInfo.getConnection().createStatement().executeUpdate("create index on " + datasetInfo.getTableName() + " ( startdate)");
+            datasetInfo.getConnection().createStatement().executeUpdate("create index on " + datasetInfo.getTableName() + " ( enddate)");
+            datasetInfo.getConnection().createStatement().executeUpdate("create index on " + datasetInfo.getTableName() + " ( startYear)");
+            datasetInfo.getConnection().createStatement().executeUpdate("create index on " + datasetInfo.getTableName() + " ( endYear)");
+            datasetInfo.getConnection().createStatement().executeUpdate("create index on " + datasetInfo.getTableName() + " ( um)");
+
+
+        } else if ("OECD_View_BiofuelPolicy".equals(uid)) {
+
+            datasetInfo.getConnection().createStatement().executeUpdate("create index on " + datasetInfo.getTableName() + " ( commodityclass,policytype)");
+            datasetInfo.getConnection().createStatement().executeUpdate("create index on " + datasetInfo.getTableName() + " ( commodityclass,policymeasure)");
+            datasetInfo.getConnection().createStatement().executeUpdate("create index on " + datasetInfo.getTableName() + " ( policymeasure,country)");
+            datasetInfo.getConnection().createStatement().executeUpdate("create index on " + datasetInfo.getTableName() + " ( policytype,country)");
+            datasetInfo.getConnection().createStatement().executeUpdate("create index on " + datasetInfo.getTableName() + " ( startdate)");
+            datasetInfo.getConnection().createStatement().executeUpdate("create index on " + datasetInfo.getTableName() + " ( enddate)");
+            datasetInfo.getConnection().createStatement().executeUpdate("create index on " + datasetInfo.getTableName() + " ( startYear)");
+            datasetInfo.getConnection().createStatement().executeUpdate("create index on " + datasetInfo.getTableName() + " ( endYear)");
+
+        } else if("OECD_Timeseries_BiofuelPolicy".equals(uid) || "OECD_Timeseries_ExportRestrictions".equals(uid)) {
+
+
+            datasetInfo.getConnection().createStatement().executeUpdate("create index on " + datasetInfo.getTableName() + " ( day,policytype, country)");
+            datasetInfo.getConnection().createStatement().executeUpdate("create index on " + datasetInfo.getTableName() + " ( day,policymeasure, country)");
+            datasetInfo.getConnection().createStatement().executeUpdate("create index on " + datasetInfo.getTableName() + " ( commodityclass)");
+            datasetInfo.getConnection().createStatement().executeUpdate("create index on " + datasetInfo.getTableName() + " ( commodityclass,policytype)");
+
+            datasetInfo.getConnection().createStatement().executeUpdate("create index on " + datasetInfo.getTableName() + " ( policytype)");
+            datasetInfo.getConnection().createStatement().executeUpdate("create index on " + datasetInfo.getTableName() + " ( policytype,policymeasure)");
+            datasetInfo.getConnection().createStatement().executeUpdate("create index on " + datasetInfo.getTableName() + " ( policymeasure)");
+            datasetInfo.getConnection().createStatement().executeUpdate("create index on " + datasetInfo.getTableName() + " ( country)");
+        }
         return false;
     }
 
--- fenix-D3S-policy/src/main/java/org/fao/policy/d3s/Query.java
+++ fenix-D3S-policy/src/main/java/org/fao/policy/d3s/Query.java
@@ -84,8 +84,9 @@ public enum Query {
                     "units,\n" +
                     "to_number(to_char(start_date,'YYYYMMDD'), '99999999') as start_date,\n" +
                     "to_number(to_char(end_date,'YYYYMMDD'), '99999999') as end_date,\n" +
-                    "to_number(to_char(start_date,'YYYY'), '9999') as beginning_year\n" +
-                    "from mastertable, policytable, commlistwithid where mastertable.cpl_id = policytable.cpl_id AND commlistwithid.commodity_id = policyTableViewTest.commodity_id;\n"
+                    "to_number(to_char(start_date,'YYYY'), '9999') as beginning_year,\n" +
+                    "to_number(to_char(end_date,'YYYY'), '9999') as ending_year\n" +
+                    "from mastertable, policytable, commlistwithid where mastertable.cpl_id = policytable.cpl_id AND commlistwithid.commodity_id = policytable.commodity_id;\n"
             ),
     OECD_View_BiofuelPolicy
             (
@@ -98,9 +99,146 @@ public enum Query {
                     "to_char(mastertable.country_code, 'FM999999999999999999') as country_code,\n"+
                     "to_number(to_char(start_date,'YYYYMMDD'), '99999999') as start_date,\n" +
                     "to_number(to_char(end_date,'YYYYMMDD'), '99999999') as end_date,\n" +
-                    "to_number(to_char(start_date,'YYYY'), '9999') as beginning_year\n" +
+                    "to_number(to_char(start_date,'YYYY'), '9999') as beginning_year,\n" +
+                    "to_number(to_char(end_date,'YYYY'), '9999') as ending_year\n" +
                     "from mastertable, policyTableViewTest where mastertable.cpl_id = policyTableViewTest.cpl_id;\n"
-            );
+            ),
+    
+    OECD_Timeseries_BiofuelPolicy (
+          "with timeseries as ( \n" +
+                  "                   select to_number(to_char(generate_series('2011-01-01'::timestamp, '2014-12-31'::timestamp, '1 day'), \n" +
+                  "                   'YYYYMMDD'),'99999999') \n" +
+                  "                   as  day ) \n" +
+                  "                    \n" +
+                  "                   select \n" +
+                  "                   final.commodityclass, \n" +
+                  "                   final.policytype, \n" +
+                  "                   final.policymeasure,  \n" +
+                  "                   final.country, \n" +
+                  "                   final.day\n" +
+                  "                 \n" +
+                  "                   from \n" +
+                  "                   (select * \n" +
+                  "                    from \n" +
+                  "                    timeseries  \n" +
+                  "                   left join \n" +
+                  "                   (select  \n" +
+                  "                   to_char(m.commodityclass_code, 'FM999999999999999999') as commodityclass,  \n" +
+                  "                   to_char(m.policytype_code, 'FM999999999999999999') as policytype,  \n" +
+                  "                   to_char(m.policymeasure_code, 'FM999999999999999999') as policymeasure,  \n" +
+                  "                   to_char(m.country_code, 'FM999999999999999999') as country, \n" +
+                  "                   to_number(to_char(start_date,'YYYYMMDD'), '99999999') as start_date,  \n" +
+                  "                   to_number(to_char(end_date,'YYYYMMDD'), '99999999') as end_date \n" +
+                  "                   from  \n" +
+                  "                   (select * from mastertable where \n" +
+                  "                   policytype_code in ('8','10','1','2','12','9') \n" +
+                  "                   and commodityclass_code IN (5,6,7))m,  \n" +
+                  "                   policytable,  \n" +
+                  "                   commlistwithid  \n" +
+                  "                   where m.cpl_id = policytable.cpl_id  \n" +
+                  "                   AND  \n" +
+                  "                   commlistwithid.commodity_id = policytable.commodity_id \n" +
+                  "                    \n" +
+                  "                   group by  \n" +
+                  "                    \n" +
+                  "                   commodityclass, \n" +
+                  "                   policytype,  \n" +
+                  "                   policymeasure,  \n" +
+                  "                   country, \n" +
+                  "                   start_date,  \n" +
+                  "                   end_date  \n" +
+                  "                   order by  \n" +
+                  "                   commodityclass, \n" +
+                  "                   policytype,  \n" +
+                  "                   policymeasure,  \n" +
+                  "                   country, \n" +
+                  "                   start_date,  \n" +
+                  "                   end_date  \n" +
+                  "                   )h \n" +
+                  "                   on \n" +
+                  "                   ((h.end_date is null AND day >= h.start_date) OR  \n" +
+                  "                   (day  between h.start_date and h.end_date) ))  final \n" +
+                  "                   group by  \n" +
+                  "                   final.commodityclass, \n" +
+                  "                   final.policytype, \n" +
+                  "                   final.policymeasure,  \n" +
+                  "                   final.country, \n" +
+                  "                   final.day \n" +
+                  "                   order by  \n" +
+                  "                   final.commodityclass, \n" +
+                  "                   final.policytype, \n" +
+                  "                   final.policymeasure,  \n" +
+                  "                   final.country, \n" +
+                  "                   final.day "
+    ),
+    OECD_Timeseries_ExportRestrictions (
+                    " with timeseries as ( \n" +
+                    "                   select to_number(to_char(generate_series('2007-01-01'::timestamp, '2014-12-31'::timestamp, '1 day'), \n" +
+                    "                   'YYYYMMDD'),'99999999') \n" +
+                    "                   as  day ) \n" +
+                    "                    \n" +
+                    "                   select \n" +
+                    "                   final.commodityclass, \n" +
+                    "                   final.policytype, \n" +
+                    "                   final.policymeasure,  \n" +
+                    "                   final.country, \n" +
+                    "                   final.day\n" +
+                    "                 \n" +
+                    "                   from \n" +
+                    "                   (select * \n" +
+                    "                    from \n" +
+                    "                    timeseries  \n" +
+                    "                    left join \n" +
+                    "                   (select  \n" +
+                    "                   to_char(m.commodityclass_code, 'FM999999999999999999') as commodityclass,  \n" +
+                    "                   to_char(m.policytype_code, 'FM999999999999999999') as policytype,  \n" +
+                    "                   to_char(m.policymeasure_code, 'FM999999999999999999') as policymeasure,  \n" +
+                    "                   to_char(m.country_code, 'FM999999999999999999') as country, \n" +
+                    "                   to_number(to_char(start_date,'YYYYMMDD'), '99999999') as start_date,  \n" +
+                    "                   to_number(to_char(end_date,'YYYYMMDD'), '99999999') as end_date \n" +
+                    "                   from  \n" +
+                    "                   (select * from mastertable where \n" +
+                    "                   policytype_code in ('1') \n" +
+                    "                   and commodityclass_code IN (1,2,3,4)" +
+                    "                   and policymeasure_code IN('1','2','4','5','8'))m,  \n" +
+                    "                   policytable,  \n" +
+                    "                   commlistwithid  \n" +
+                    "                   where m.cpl_id = policytable.cpl_id  \n" +
+                    "                   AND  \n" +
+                    "                   commlistwithid.commodity_id = policytable.commodity_id \n" +
+                    "                    \n" +
+                    "                   group by  \n" +
+                    "                    \n" +
+                    "                   commodityclass, \n" +
+                    "                   policytype,  \n" +
+                    "                   policymeasure,  \n" +
+                    "                   country, \n" +
+                    "                   start_date,  \n" +
+                    "                   end_date  \n" +
+                    "                   order by  \n" +
+                    "                   commodityclass, \n" +
+                    "                   policytype,  \n" +
+                    "                   policymeasure,  \n" +
+                    "                   country, \n" +
+                    "                   start_date,  \n" +
+                    "                   end_date  \n" +
+                    "                   )h \n" +
+                    "                   on \n" +
+                    "                   ((h.end_date is null AND day >= h.start_date) OR  \n" +
+                    "                   (day  between h.start_date and h.end_date) ))  final \n" +
+                    "                   group by  \n" +
+                    "                   final.commodityclass, \n" +
+                    "                   final.policytype, \n" +
+                    "                   final.policymeasure,  \n" +
+                    "                   final.country, \n" +
+                    "                   final.day \n" +
+                    "                   order by  \n" +
+                    "                   final.commodityclass, \n" +
+                    "                   final.policytype, \n" +
+                    "                   final.policymeasure,  \n" +
+                    "                   final.country, \n" +
+                    "                   final.day \n"
+    );
 
     private String query;
     Query(String query) {
