525f863bf27d4d244289316a39fd6242b0f45544
fabriziofenix <fabrizio.castelli@fao.org>
2017-03-15 12:20:04 +0100

    adam dao for indicators

--- fenix-D3S-adam/src/main/java/org/fao/ess/adam/d3s/Adam.java
+++ fenix-D3S-adam/src/main/java/org/fao/ess/adam/d3s/Adam.java
@@ -14,10 +14,11 @@ import java.util.*;
 
 public class Adam extends WDSDatasetDao {
 
-    @Inject private DataSource dataSource;
-    @Inject private DatabaseUtils databaseUtils;
+    @Inject
+    private DataSource dataSource;
+    @Inject
+    private DatabaseUtils databaseUtils;
     private boolean initialized = false;
-
 
 
     @Override
@@ -28,33 +29,47 @@ public class Adam extends WDSDatasetDao {
     @Override
     public void init(Map<String, String> properties) throws Exception {
         if (!initialized)
-            dataSource.init(properties.get("url"),properties.get("usr"),properties.get("psw"));
+            dataSource.init(properties.get("url"), properties.get("usr"), properties.get("psw"));
         initialized = true;
     }
 
     @Override
     public Iterator<Object[]> loadData(MeIdentification resource) throws Exception {
-       /* DSD dsd = resource!=null ? resource.getDsd() : null;
-        Collection<DSDColumn> columns = dsd!=null && dsd instanceof DSDDataset ? ((DSDDataset)dsd).getColumns() : null;
-        if (columns==null)
-            throw new Exception("Wrong table structure");
 
-        String topic = getAmountType(resource.getUid());
-        if (topic==null)
-            return new LinkedList<Object[]>().iterator();
+        Iterator<Object[]> iterator;
 
-        Connection connection = dataSource.getConnection();
-        connection.setAutoCommit(false);
-        Statement statement = connection.createStatement(java.sql.ResultSet.TYPE_FORWARD_ONLY, java.sql.ResultSet.CONCUR_READ_ONLY);
-        statement.setFetchSize(1000);*/
+        switch (resource.getUid()) {
 
-        return new LinkedList<Object[]>().iterator();
-     /*   return new DataIterator(
-                statement.executeQuery(buildQuery(columns, topic)),
-                connection,
-                null,
-                buildNulls(columns)
-        );*/
+            case "adam_combined_priorities_table":
+            case "adam_country_indicators":
+            case "adam_donors_gni":
+
+                DSD dsd = resource != null ? resource.getDsd() : null;
+                Collection<DSDColumn> columns = dsd != null && dsd instanceof DSDDataset ? ((DSDDataset) dsd).getColumns() : null;
+                if (columns == null)
+                    throw new Exception("Wrong table structure");
+
+                String topic = getAmountType(resource.getUid());
+                if (topic == null)
+                    return new LinkedList<Object[]>().iterator();
+
+                Connection connection = dataSource.getConnection();
+                connection.setAutoCommit(false);
+                Statement statement = connection.createStatement(java.sql.ResultSet.TYPE_FORWARD_ONLY, java.sql.ResultSet.CONCUR_READ_ONLY);
+                statement.setFetchSize(1000);
+                iterator = new DataIterator(
+                        statement.executeQuery(buildQuery(columns, topic)),
+                        connection,
+                        null,
+                        buildNulls(columns));
+                break;
+
+            default:
+
+                iterator = new LinkedList<Object[]>().iterator();
+                break;
+        }
+        return iterator;
     }
 
     @Override
@@ -69,30 +84,28 @@ public class Adam extends WDSDatasetDao {
     }
 
 
-
     //Utils
     private String getAmountType(String uid) {
-        uid = uid!=null ? uid.replace('.','_').toLowerCase() : null;
-        return uid!=null && uid.startsWith("adam_") ? uid.substring("adam_".length()) : null;
+        uid = uid != null ? uid.replace('.', '_').toLowerCase() : null;
+        return uid != null && uid.startsWith("adam_") ? uid.substring("adam_".length()) : null;
     }
 
     private Object[] buildNulls(Collection<DSDColumn> columns) {
-        int i=0;
+        int i = 0;
         Object[] nullValues = new Object[columns.size()];
         for (DSDColumn column : columns)
-            nullValues[i++] = column.getKey()!=null && column.getKey() ? "NA" : null;
+            nullValues[i++] = column.getKey() != null && column.getKey() ? "NA" : null;
         return nullValues;
     }
 
-    private String buildQuery(Collection<DSDColumn> columns, String amounttype   ) {
+    private String buildQuery(Collection<DSDColumn> columns, String amounttype) {
         StringBuilder select = new StringBuilder();
 
         for (DSDColumn column : columns)
             select.append(',').append(column.getId());
 
-        return "select "+select.substring(1)+" FROM "+amounttype;
+        return "select " + select.substring(1) + " FROM " + amounttype;
     }
-
 
 
 }
