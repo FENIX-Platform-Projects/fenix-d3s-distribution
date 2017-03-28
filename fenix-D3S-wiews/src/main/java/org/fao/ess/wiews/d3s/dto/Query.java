bb61ac1d817c81dad16aaf8143c4d79d676170a9
fabriziofenix <fabrizio.castelli@fao.org>
2017-03-28 12:33:49 +0200

    query for table + codelists

--- fenix-D3S-wiews/src/main/java/org/fao/ess/wiews/d3s/dto/Query.java
+++ fenix-D3S-wiews/src/main/java/org/fao/ess/wiews/d3s/dto/Query.java
@@ -4,26 +4,34 @@ public enum Query {
 
 
     indicator20 ("SELECT\n" +
-            "\n" +
             "  cast(ITERATION as CHAR(50)) as iteration,\n" +
-            "\n" +
-            "\n" +
-            "       '2240' as domain,\n" +
-            "       'stk' as element,\n" +
-            "       '20' as indicator,\n" +
-            "       cast(a.biologicalAccessionId as CHAR(50)) as biologicalAccessionId,\n" +
-            "       b.ISO as country,\n" +
-            "       (SELECT it.WIEWS_INSTCODE FROM ref_instab it WHERE  it.id = a.orgId) as stakeholder,\n" +
-            "       count(distinct(SELECT IF (a.taxonId > 0, a.taxonId, a.taxon_freetext))) as value,\n" +
-            "       '1' as um\n" +
-            "FROM answer_q14 a\n" +
-            "  JOIN ref_country b ON a.Country_ID = b.COUNTRY_ID\n" +
-            "WHERE a.approved = true\n" +
-            "GROUP BY iteration, stakeholder, country\n" +
+            "  '2240' as domain,\n" +
+            "  'stk' as element,\n" +
+            "  '20' as indicator,\n" +
+            "  cast(a.biologicalAccessionId as CHAR(50)) as biologicalAccessionId,\n" +
+            "  b.ISO as country,\n" +
+            "  b.ISO as m49_country,\n" +
+            "  c.WIEWS_INSTCODE as stakeholder,\n" +
+            "  'na' as genus,\n" +
+            "  count(distinct(SELECT IF (a.taxonId > 0, a.taxonId, a.taxon_freetext))) as value,\n" +
+            "  '1' as um\n" +
+            "FROM\n" +
+            "  answer_q14 a\n" +
+            "  JOIN\n" +
+            "  ref_country b\n" +
+            "    ON a.Country_ID = b.COUNTRY_ID\n" +
+            "  JOIN\n" +
+            "  ref_instab c\n" +
+            "    on a.orgId = c.ID\n" +
+            "WHERE\n" +
+            "  a.approved = true\n" +
+            "GROUP BY\n" +
+            "  iteration,\n" +
+            "  stakeholder,\n" +
+            "  country\n" +
             "\n" +
             "UNION\n" +
-            "\n" +
-            "\n" +
+            "/* By rating*/\n" +
             "\n" +
             "SELECT\n" +
             "  cast(iteration as CHAR(50)) as iteration,\n" +
@@ -32,42 +40,138 @@ public enum Query {
             "  '20' as indicator,\n" +
             "  cast(null as CHAR(50)) as biologicalAccessionId,\n" +
             "  country,\n" +
+            "  country as m49_country,\n" +
             "  'ZZZ' as stakeholder,\n" +
-            "  nfp_rating as VALUE ,\n" +
+            "  'na' as genus,\n" +
+            "  nfp_rating as VALUE,\n" +
             "  '%' as um\n" +
-            "from ( SELECT\n" +
-            "         b.ISO as country,\n" +
-            "         a.applicable,\n" +
-            "         a.data_available,\n" +
-            "         a.id,\n" +
-            "         a.nfp_rating,\n" +
-            "         a.iteration,\n" +
-            "         a.comment\n" +
-            "       FROM indicator_analysis a\n" +
-            "         JOIN ref_country b\n" +
-            "           ON (a.country_id = b.COUNTRY_ID)\n" +
-            "       WHERE indicator_id = 20)t\n" +
-            "GROUP BY iteration, domain, element, country\n" +
-            "\n" +
+            "from\n" +
+            "  (\n" +
+            "    SELECT\n" +
+            "      b.ISO as country,\n" +
+            "      a.applicable,\n" +
+            "      a.data_available,\n" +
+            "      a.id,\n" +
+            "      a.nfp_rating,\n" +
+            "      a.iteration,\n" +
+            "      a.comment\n" +
+            "    FROM\n" +
+            "      indicator_analysis a\n" +
+            "      JOIN\n" +
+            "      ref_country b\n" +
+            "        ON (a.country_id = b.COUNTRY_ID)\n" +
+            "    WHERE\n" +
+            "      indicator_id = 20\n" +
+            "  )\n" +
+            "  t\n" +
+            "GROUP BY\n" +
+            "  iteration,\n" +
+            "  domain,\n" +
+            "  element,\n" +
+            "  country\n" +
             "\n" +
             "union\n" +
+            "/* By country*/\n" +
             "\n" +
             "SELECT\n" +
-            "  cast(ITERATION as CHAR(50)) as iteration,\n" +
-            "\n" +
+            "  iteration,\n" +
             "  '2240' as domain,\n" +
             "  'ind' as element,\n" +
             "  '20' as indicator,\n" +
             "  cast(null as CHAR(50)) as biologicalAccessionId,\n" +
-            "  b.ISO as country,\n" +
+            "  country,\n" +
+            "   m49_country,\n" +
             "  'ZZZ' as stakeholder,\n" +
-            "\n" +
-            "  count(*) as value,\n" +
+            "  'na' as genus,\n" +
+            "  sum(value) as VALUE,\n" +
             "  '1' as um\n" +
+            "FROM\n" +
+            "  (\n" +
+            "    SELECT\n" +
+            "      cast(ITERATION as CHAR(50)) as iteration,\n" +
+            "      '2240' as domain,\n" +
+            "      'ind' as element,\n" +
+            "      '20' as indicator,\n" +
+            "      cast(a.biologicalAccessionId as CHAR(50)) as biologicalAccessionId,\n" +
+            "      b.ISO as country,\n" +
+            "      b.ISO as m49_country,\n" +
+            "      'ZZZ' as stakeholder,\n" +
+            "      'na' as genus,\n" +
+            "      count(distinct(SELECT IF (a.taxonId > 0, a.taxonId, a.taxon_freetext))) as value,\n" +
+            "      '1' as um\n" +
+            "    FROM\n" +
+            "      answer_q14 a\n" +
+            "      JOIN\n" +
+            "      ref_country b\n" +
+            "        ON a.Country_ID = b.COUNTRY_ID\n" +
+            "      JOIN\n" +
+            "      ref_instab c\n" +
+            "        on a.orgId = c.ID\n" +
+            "    WHERE\n" +
+            "      a.approved = true\n" +
+            "      AND c.WIEWS_INSTCODE not in\n" +
+            "          (\n" +
+            "            'BEL084',\n" +
+            "            'CIV033',\n" +
+            "            'COL003',\n" +
+            "            'ETH013',\n" +
+            "            'IND002',\n" +
+            "            'KEN056',\n" +
+            "            'MEX002',\n" +
+            "            'NGA039',\n" +
+            "            'PER001',\n" +
+            "            'PHL001',\n" +
+            "            'SYR002',\n" +
+            "            'TWN001',\n" +
+            "            'FJI049',\n" +
+            "            'SWE054'\n" +
+            "          )\n" +
+            "    GROUP BY\n" +
+            "      iteration,\n" +
+            "      stakeholder,\n" +
+            "      country\n" +
+            "  )\n" +
+            "  z\n" +
+            "GROUP BY\n" +
+            "  iteration,\n" +
+            "  domain,\n" +
+            "  element,\n" +
+            "  country\n" +
             "\n" +
-            "FROM answer_q14 a\n" +
-            "  JOIN ref_country b ON a.Country_ID = b.COUNTRY_ID\n" +
-            "GROUP BY iteration,country\n" );
+            "UNION\n" +
+            " /* By genus*/\n" +
+            "SELECT\n" +
+            "  cast(ITERATION as CHAR(50)) as iteration,\n" +
+            "  '2240' as domain,\n" +
+            "  'gen' as element,\n" +
+            "  '20' as indicator,\n" +
+            "  cast(a.biologicalAccessionId as CHAR(50)) as biologicalAccessionId,\n" +
+            "  b.ISO as country,\n" +
+            "  b.ISO as  m49_country,\n" +
+            "  d.WIEWS_INSTCODE as stakeholder,\n" +
+            "  lower(COALESCE(c.GENUS,'NA')) as genus,\n" +
+            "  count(distinct(\n" +
+            "    SELECT\n" +
+            "      IF (a.taxonId > 0, a.taxonId, a.taxon_freetext))) as value,\n" +
+            "  '1' as um\n" +
+            "FROM\n" +
+            "  answer_q14 a\n" +
+            "  JOIN\n" +
+            "  ref_country b\n" +
+            "    ON a.Country_ID = b.COUNTRY_ID\n" +
+            "  JOIN\n" +
+            "  ref_taxtab c\n" +
+            "    on a.taxonId = c.TAXON_ID\n" +
+            "  JOIN\n" +
+            "  ref_instab d\n" +
+            "    on d.ID = a.orgId\n" +
+            "WHERE\n" +
+            "  a.approved = true\n" +
+            "GROUP BY\n" +
+            "  iteration,\n" +
+            "  stakeholder,\n" +
+            "  country,\n" +
+            "  genus" );
 
     private String query;
     Query(String query) {
