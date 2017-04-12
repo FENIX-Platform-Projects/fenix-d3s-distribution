98844a34c2c457e5f79f7c30c658e4541eed1fff
ivanocortesini <ivano.cortesini@gmail.com>
2017-04-12 12:25:39 +0200

    ...

---
+++ fenix-D3S-wiews/src/main/java/org/fao/fenix/d3p/process/dto/WiewsRegionFilterParams.java
@@ -0,0 +1,9 @@
+package org.fao.fenix.d3p.process.dto;
+
+import org.fao.fenix.commons.find.dto.filter.StandardFilter;
+
+public class WiewsRegionFilterParams {
+    public StandardFilter filter;
+    public boolean total = true;
+    public boolean list = true;
+}
--- fenix-D3S-wiews/src/main/java/org/fao/fenix/d3p/process/impl/RegionFiltering.java
+++ fenix-D3S-wiews/src/main/java/org/fao/fenix/d3p/process/impl/RegionFiltering.java
@@ -9,7 +9,9 @@ import org.fao.fenix.commons.msd.dto.full.DSDDataset;
 import org.fao.fenix.commons.utils.database.DatabaseUtils;
 import org.fao.fenix.d3p.dto.*;
 import org.fao.fenix.d3p.process.dto.InternationalGeneBanks;
+import org.fao.fenix.d3p.process.dto.WiewsRegionFilterParams;
 import org.fao.fenix.d3p.process.type.ProcessName;
+import org.fao.fenix.d3s.cache.dto.dataset.Column;
 
 import javax.inject.Inject;
 import javax.ws.rs.BadRequestException;
@@ -19,110 +21,118 @@ import java.sql.ResultSet;
 import java.util.*;
 
 @ProcessName("wiews_area_filter")
-public class RegionFiltering extends org.fao.fenix.d3p.process.Process<StandardFilter> {
+public class RegionFiltering extends org.fao.fenix.d3p.process.Process<WiewsRegionFilterParams> {
     @Inject DatabaseUtils databaseUtils;
     @Inject StepFactory stepFactory;
 
     private static final String internationalGeneBanksCode = "WITC";
 
     @Override
-    public Step process(StandardFilter params, Step[] sourceStep) throws Exception {
+    public Step process(WiewsRegionFilterParams params, Step[] sourceStep) throws Exception {
         //Retrieve source information
-        Step source = sourceStep!=null && sourceStep.length==1 ? sourceStep[0] : null;
-        StepType type = source!=null ? source.getType() : null;
-        if (type==null || (type!=StepType.table && type!=StepType.query))
-            throw new BadRequestException("wiews_area_filter process can be applied only on a table or an other select query");
-        String tableName = type==StepType.table ? (String)source.getData() : '('+(String)source.getData()+") as " + source.getRid();
-        DSDDataset dsd = source.getDsd();
+        Step sourceMapping = sourceStep!=null && sourceStep.length>0 ? sourceStep[0] : null;
+        StepType typeSourceMapping = sourceMapping!=null ? sourceMapping.getType() : null;
+        Step sourceCountries = sourceStep!=null && sourceStep.length>1 ? sourceStep[1] : null;
+        StepType typeSourceCountries = sourceCountries!=null ? sourceCountries.getType() : null;
+        if (typeSourceMapping==null || typeSourceCountries==null || (typeSourceMapping!=StepType.table && typeSourceMapping!=StepType.query) || (typeSourceCountries!=StepType.table && typeSourceCountries!=StepType.query))
+            throw new BadRequestException("wiews_area_filter process requires two table or query sources to be applied: w_regions_mapping and w_regions_countries");
+        String tableNameMapping = typeSourceMapping==StepType.table ? (String)sourceMapping.getData() : '('+(String)sourceMapping.getData()+") as " + sourceMapping.getRid();
+        String tableNameCountries = typeSourceCountries==StepType.table ? (String)sourceCountries.getData() : '('+(String)sourceCountries.getData()+") as " + sourceCountries.getRid();
+        DSDDataset dsdMapping = sourceMapping.getDsd();
+        DSDDataset dsdCountries = sourceCountries.getDsd();
         //Create countries variable
-        setGlobalVariable("required_countries", getCountriesCode(source, type, params, tableName, dsd));
+        Collection<String> list = new LinkedList<>();
+        Map<String, Collection<String>> columnsCodes = getColumnsCodes(params.filter, dsdMapping);
+        Collection<String> regions = getRegionCodes(tableNameMapping, columnsCodes, sourceMapping);
+        if (params.total)
+            list.addAll(regions);
+        if (params.list)
+            list.addAll(getCountriesCode(tableNameCountries, columnsCodes, regions, sourceCountries));
+        setGlobalVariable("required_countries", list.toArray());
         //Create international gene banks variable when needed (to exclude them)
-        //if (!includeInternationalGeneBanks(params))
-            setGlobalVariable("stakeholders_exclusion", getInternationalGeneBanks());
+        setGlobalVariable("stakeholders_exclusion", getInternationalGeneBanks());
         //Return ghost step
-        Step step = stepFactory.getInstance(type);
-        step.setDsd(dsd);
-        step.setData(source.getData());
-        if (type==StepType.query)
-            ((QueryStep)step).setParams(((QueryStep)source).getParams());
+        Step step = stepFactory.getInstance(typeSourceMapping);
+        step.setDsd(dsdMapping);
+        step.setData(sourceMapping.getData());
+        if (typeSourceMapping==StepType.query)
+            ((QueryStep)step).setParams(((QueryStep)sourceMapping).getParams());
         return step;
     }
 
-    private Object[] getCountriesCode (Step source, StepType type, StandardFilter params, String tableName, DSDDataset dsd) throws Exception {
-        Collection<String> countries = new LinkedList<>();
 
-        Object[] existingParams = type==StepType.query ? ((QueryStep)source).getParams() : null;
-        Collection<Object> queryParams = existingParams!=null ? new LinkedList<>(Arrays.asList(existingParams)) : new LinkedList<>();
-        String query = createQuery(getColumnsCodes(params, dsd),tableName,queryParams);
 
+
+    private Collection<String> getCountriesCode (String tableName, Map<String, Collection<String>> columnsCodes, Collection<String> regions, Step source) throws Exception {
+        //Create query
+        Collection<Object> queryParams = new LinkedList<>();
+        StringBuilder queryBuffer = new StringBuilder("select distinct (country_iso3) from ").append(tableName);
+        if (columnsCodes.size()>0) {
+            queryBuffer.append(" where");
+            if (regions.size()>0) {
+                queryBuffer.append(' ').append("w").append(" in (");
+                for (int i=0, l=regions.size(); i<l; i++)
+                    queryBuffer.append("?,");
+                queryBuffer.setCharAt(queryBuffer.length()-1,')');
+                queryBuffer.append(" or");
+                queryParams.addAll(regions);
+            }
+            for (Map.Entry<String, Collection<String>> columnCodesEntry : columnsCodes.entrySet())
+                if (columnCodesEntry.getKey().equals("iso3")) {
+                    queryBuffer.append(' ').append("country_iso3").append(" in (");
+                    for (int i=0, l=columnCodesEntry.getValue().size(); i<l; i++)
+                        queryBuffer.append("?,");
+                    queryBuffer.setCharAt(queryBuffer.length()-1,')');
+                    queryBuffer.append(" or");
+                    queryParams.addAll(columnCodesEntry.getValue());
+                }
+        }
+        String query = columnsCodes.size()>0 ? queryBuffer.substring(0, queryBuffer.length()-3) : queryBuffer.toString();
+        //Parse and return result
         Connection connection = source.getStorage().getConnection();
         try {
-            PreparedStatement statement = connection.prepareStatement(query);
-            databaseUtils.fillStatement(statement,null,queryParams.toArray());
-            for (ResultSet resultSet = statement.executeQuery(); resultSet.next(); )
+            Collection<String> countries = new LinkedList<>();
+            for (ResultSet resultSet = select(query, queryParams, connection, source); resultSet.next(); )
                 countries.add(resultSet.getString(1));
+            return countries;
         } finally {
             connection.close();
         }
-
-        return addWorldCodes(countries, params).toArray();
     }
 
-    private Collection<String> addWorldCodes(Collection<String> countries, StandardFilter params) {
-        boolean add_w = false;
-        boolean add_w_i = false;
-        if (params!=null)
-            for (Map.Entry<String, FieldFilter> paramsEntry : params.entrySet()) {
-                String worldCode = null;
-                if (paramsEntry.getKey().equals("m49") || paramsEntry.getKey().equals("mdg") || paramsEntry.getKey().equals("sdg"))
-                    worldCode = "1";
-                else if (paramsEntry.getKey().equals("fao"))
-                    worldCode = "5000";
-                else if (paramsEntry.getKey().equals("itpgrfa") || paramsEntry.getKey().equals("cgrfa"))
-                    worldCode = "0";
+    private Collection<String> getRegionCodes(String tableName, Map<String, Collection<String>> columnsCodes, Step source) throws Exception {
+        Collection<String> regions = new LinkedList<>();
 
-                for (CodesFilter codesFilter : paramsEntry.getValue().codes) {
-                    add_w_i |= codesFilter.codes.contains(internationalGeneBanksCode);
-                    add_w |= codesFilter.codes.contains(worldCode);
-                }
-            }
-
-        if (add_w)
-            countries.add("1");
-        if (add_w_i)
-            countries.add(internationalGeneBanksCode);
-
-        return countries;
-    }
-
-
-    private String createQuery(Map<String, Collection<String>> columnsCodes, String tableName, Collection<Object> params) {
-        StringBuilder query = new StringBuilder("select distinct (m49_country) as m49_country from ").append(tableName);
         if (columnsCodes.size()>0) {
-            query.append(" where");
+            //Create query
+            Collection<Object> queryParams = new LinkedList<>();
+            StringBuilder queryBuffer = new StringBuilder("select distinct (w) from ").append(tableName).append(" where");
             for (Map.Entry<String, Collection<String>> columnCodesEntry : columnsCodes.entrySet()) {
-                query.append(' ').append(columnCodesEntry.getKey()).append(" in (");
+                queryBuffer.append(' ').append(columnCodesEntry.getKey()).append(" in (");
                 for (int i=0, l=columnCodesEntry.getValue().size(); i<l; i++)
-                    query.append("?,");
-                query.setCharAt(query.length()-1,')');
-                query.append(" or");
-                params.addAll(columnCodesEntry.getValue());
+                    queryBuffer.append("?,");
+                queryBuffer.setCharAt(queryBuffer.length()-1,')');
+                queryBuffer.append(" or");
+                queryParams.addAll(columnCodesEntry.getValue());
+            }
+            String query = queryBuffer.substring(0, queryBuffer.length()-3);
+            //Parse result
+            Connection connection = source.getStorage().getConnection();
+            try {
+                for (ResultSet resultSet = select(query, queryParams, connection, source); resultSet.next(); )
+                    regions.add(resultSet.getString(1));
+            } finally {
+                connection.close();
             }
         }
-        return columnsCodes.size()>0 ? query.substring(0, query.length()-3) : query.toString();
-    }
 
+        return regions;
+    }
 
     private Map<String, Collection<String>> getColumnsCodes(StandardFilter params, DSDDataset dsd) throws Exception {
         Map<String, Collection<String>> columnsCodes = new HashMap<>();
         if (params!=null) {
             for (Map.Entry<String, FieldFilter> paramsEntry : params.entrySet()) {
-                Collection<String> columnsId = new LinkedList<>();
-                for (DSDColumn column : dsd.getColumns())
-                    if (column.getId().startsWith(paramsEntry.getKey()))
-                        columnsId.add(column.getId());
-                if (columnsId.size() == 0)
-                    throw new BadRequestException("No correspondence found for column " + paramsEntry.getKey() + " into region-country mapping dataset");
 
                 if (paramsEntry.getValue().retrieveFilterType() != FieldFilterType.code)
                     throw new BadRequestException("Wrong filter type for column " + paramsEntry.getKey() + ". Only codes can be used");
@@ -132,23 +142,11 @@ public class RegionFiltering extends org.fao.fenix.d3p.process.Process<StandardF
                     if (codesFilter.codes != null)
                         codes.addAll(codesFilter.codes);
                 if (codes.size() > 0)
-                    for (String columnId : columnsId)
-                        columnsCodes.put(columnId, codes);
+                    columnsCodes.put(paramsEntry.getKey(), codes);
             }
         }
         return columnsCodes;
     }
-
-
-    private boolean includeInternationalGeneBanks(StandardFilter params) {
-        if (params!=null)
-            for (Map.Entry<String, FieldFilter> paramsEntry : params.entrySet())
-                for (CodesFilter codesFilter : paramsEntry.getValue().codes)
-                    if (codesFilter.codes.contains(internationalGeneBanksCode))
-                        return true;
-        return false;
-    }
-
 
     private Object[] getInternationalGeneBanks() {
         Collection internationalGeneBanks = new LinkedList();
@@ -156,4 +154,19 @@ public class RegionFiltering extends org.fao.fenix.d3p.process.Process<StandardF
             internationalGeneBanks.add(bank.name());
         return internationalGeneBanks.toArray();
     }
+
+
+
+    //Utils
+    private ResultSet select (String query, Collection params, Connection connection, Step source) throws Exception {
+        Object[] existingParams = source.getType()==StepType.query ? ((QueryStep)source).getParams() : null;
+        Collection<Object> queryParams = existingParams!=null ? new LinkedList<>(Arrays.asList(existingParams)) : new LinkedList<>();
+        queryParams.addAll(params);
+
+        PreparedStatement statement = connection.prepareStatement(query);
+        databaseUtils.fillStatement(statement,null,queryParams.toArray());
+        return statement.executeQuery();
+    }
+
+
 }
