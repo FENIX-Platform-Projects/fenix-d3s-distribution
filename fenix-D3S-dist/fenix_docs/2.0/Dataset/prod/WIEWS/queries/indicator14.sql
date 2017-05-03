ec9dfedc94b690c9a453824e6a4aa0cfd198baab
fabriziofenix <fabrizio.castelli@fao.org>
2017-05-03 12:17:13 +0200

    indicator14

--- fenix-D3S-dist/fenix_docs/2.0/Dataset/prod/WIEWS/queries/indicator14.sql
+++ fenix-D3S-dist/fenix_docs/2.0/Dataset/prod/WIEWS/queries/indicator14.sql
@@ -1,52 +1,54 @@
+with raw as (
+    SELECT
+      ref_c.iso,
+      iteration,
+      c.wiews_instcode as stakeholder,
+      answer_freetext,
+      answer_freetext_local,
 
-SELECT
-
-*
-from (
-SELECT
-  a.*,
-  ad.*,
-
-  d.crop_name,
-  c.wiews_instcode,
-  ref_c.iso,
-  ref_c.name
-FROM   answer a
-  JOIN   answer_detail ad
-    ON     (a.id = answerid )
-  JOIN ref_instab c on a.orgid = c.id
-  JOIN ref_country ref_c on ref_c.country_id = a.country_id
-  FULL OUTER JOIN  ref_crop d on reference_id = crop_id::TEXT
-WHERE  questionid = 11
-AND subquestionid =1058
-and ref_c.lang = 'EN'
-  and d.lang = 'EN'
-ORDER BY iso )h
-WHERE wiews_instcode = 'ALB026'
-
-UNION
+      reference_id
 
 
-SELECT
-*
-from (
-       SELECT
-         a.*,
-         ad.*,
+    FROM
+      answer a
+      JOIN
+      answer_detail ad
+        ON ( a.id = answerid )
+      JOIN
+      ref_instab c
+        ON a.orgid = c.id
+      JOIN
+      ref_country ref_c
+        ON ref_c.country_id = a.country_id
 
-         null::TEXT as cropname,
-         c.wiews_instcode,
-         ref_c.iso,
-         ref_c.name
-       FROM   answer a
-         JOIN   answer_detail ad
-           ON     (a.id = answerid )
-         JOIN ref_instab c on a.orgid = c.id
-         JOIN ref_country ref_c on ref_c.country_id = a.country_id
-       WHERE  questionid = 11
-              AND subquestionid =1058
-              and ref_c.lang = 'EN'
-         and ad.reference_id IS null
-       ORDER BY iso )h
-WHERE wiews_instcode = 'ALB026'
+    WHERE questionid = 11 and ad.subquestionid= 1058 and ref_c.lang = 'EN' ),
 
+    test_14_indi as (
+
+
+      SELECT
+
+        iteration,
+        iso,
+        stakeholder,
+        CASE
+        WHEN reference_id is null or reference_id = '0'  and answer_freetext is not null THEN answer_freetext
+        WHEN reference_id is null or reference_id = '0'  and answer_freetext is  null THEN  answer_freetext_local
+        WHEN reference_id is not null then reference_id end as crop,
+        count(*) as sum
+
+
+      FROM
+        raw
+      GROUP BY iteration,
+        iso,
+        stakeholder,
+        crop)
+
+
+
+                         SELECT
+                           *
+                         FROM
+                           test_14_indi
+                         ORDER BY sum,iteration, iso, stakeholder
