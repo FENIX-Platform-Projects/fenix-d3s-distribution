cef77ccf675408a2c0616de8177de1b5220688aa
fabriziofenix <fabrizio.castelli@fao.org>
2017-06-28 17:32:16 +0200

    add codelist ofr country_indicators add query for project analysis download

---
+++ fenix-D3S-dist/fenix_docs/2.0/Process/ADAM/v1/dashboard/project_analysis/download/query_for_year.sql
@@ -0,0 +1,24 @@
+SELECT
+  oda_grp_en           AS CURRENT
+  recipientcode,
+  recipientcode_en     AS recipient,
+  donorcode,
+  donorcode_en         AS donor,
+  projecttitle,
+  year,
+  parentsector_code,
+  parentsector_code_en AS parentsector,
+  purposecode,
+  purposecode_en       AS purpose,
+  commitment_value,
+  disbursement_value,
+  unitcode,
+  unitcode_en          AS unit
+FROM data.adam_project_analysis
+WHERE year BETWEEN 2000 and 2015
+ORDER BY
+  year,
+  recipient,
+  donor,
+  parentsector,
+  purpose;
