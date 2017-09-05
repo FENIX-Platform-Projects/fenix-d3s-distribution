2a51b324c018a62637394439644f111e0f1d5107
Salvatore Cascone <Salvatore.Cascone@fao.org>
2017-09-05 15:36:35 +0200

    Updated Ansible scripts for D3S
    New scripts for D3S installation and some refinements.

--- fenix-D3S-dist/dist/start.sh
+++ fenix-D3S-dist/dist/start.sh
@@ -1,5 +1,5 @@
-JAVA_OPTS="-server -Xmx2g -Djna.nosys=true -XX:+HeapDumpOnOutOfMemoryError -Djava.awt.headless=true -Dfile.encoding=UTF8 -Drhino.opt.level=9"
+JAVA_OPTS="-server -Xms2g -Xmx2g -XX:PermSize=512m -XX:MaxPermSize=512m -Djna.nosys=true -XX:+HeapDumpOnOutOfMemoryError -Djava.awt.headless=true -Dfile.encoding=UTF8 -Drhino.opt.level=9"
 ORIENTDB_SETTINGS="-Dprofiler.enabled=true -Dorientdb.www.path="database/www""
 LOG_SETTINGS="-Dlog4j.configuration="file:config/logging/log4j.properties""
 
-java $JAVA_OPTS $ORIENTDB_SETTINGS $LOG_SETTINGS -jar lib/d3s.jar &
+exec -a d3s_dev java $JAVA_OPTS $ORIENTDB_SETTINGS $LOG_SETTINGS -jar lib/d3s.jar &
