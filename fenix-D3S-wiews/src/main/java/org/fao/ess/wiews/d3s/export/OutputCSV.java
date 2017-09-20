8c00618c266bfa68e1e70b1ef0c4629304801985
ivanocortesini <ivano.cortesini@gmail.com>
2017-09-20 14:17:45 +0200

    ...

---
+++ fenix-D3S-wiews/src/main/java/org/fao/ess/wiews/d3s/export/OutputCSV.java
@@ -0,0 +1,12 @@
+package org.fao.ess.wiews.d3s.export;
+
+
+@org.fao.fenix.commons.utils.annotations.export.Output("wiewsOutputCSV")
+public class OutputCSV extends org.fao.fenix.export.d3s.impl.OutputCSV {
+
+    {
+        CODE_ENUM = "(ID)";
+        LABEL_ENUM = null;
+    }
+
+}
